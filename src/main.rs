61a976148c0d8908a161d99ce58fb634af3ee8f9
Alexander Kudasov <sasha.kudasov04@gmail.com>
2021-10-30 23:14:34 +0300

    Fix? Github Actions

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use geng::prelude::*;
+
+mod game_state;
+
+fn main() {
+    logger::init().unwrap();
+    geng::setup_panic_handler();
+
+    // Setup working directory
+    if let Some(dir) = std::env::var_os("CARGO_MANIFEST_DIR") {
+        std::env::set_current_dir(std::path::Path::new(&dir).join("static")).unwrap();
+    } else {
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            if let Some(path) = std::env::current_exe().unwrap().parent() {
+                std::env::set_current_dir(path).unwrap();
+            }
+        }
+    }
+
+    let geng = Geng::new("Categories");
+    let state = game_state::GameState::new(&geng);
+
+    geng::run(&geng, state);
+}
