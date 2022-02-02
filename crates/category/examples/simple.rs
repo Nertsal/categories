2e135da1b2409a9d1ad0354b91fd6ab87312710a
Nertsal <sasha.kudasov04@gmail.com>
2022-02-02 19:57:13 +0300

    Fix example

--- crates/category/examples/simple.rs
+++ crates/category/examples/simple.rs
@@ -2,10 +2,17 @@ use category::*;
 
 fn main() {
     let mut category = Category::new();
-    let a = category.new_object(());
-    let b = category.new_object(());
+    let a = category.new_object(Object {
+        tags: vec![],
+        inner: (),
+    });
+    let b = category.new_object(Object {
+        tags: vec![],
+        inner: (),
+    });
     category.new_morphism(Morphism {
         connection: MorphismConnection::Regular { from: a, to: b },
+        tags: vec![],
         inner: (),
     });
 
