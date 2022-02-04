f19f1b74c5e19f3cb25c389e1e9450e40882b8ed
Nertsal <sasha.kudasov04@gmail.com>
2022-02-04 20:06:21 +0300

    Better constraint_equality

--- crates/category/src/rule/constraint/equality.rs
+++ crates/category/src/rule/constraint/equality.rs
@@ -6,44 +6,18 @@ pub fn constraint_equality<'a, O, M, L: Label>(
     bindings: &'a Bindings<L>,
     category: &'a Category<O, M>,
 ) -> Box<dyn Iterator<Item = Bindings<L>> + 'a> {
-    match (
-        bindings.get_morphism(morphism_f),
-        bindings.get_morphism(morphism_g),
-    ) {
-        (Some(morphism_f), Some(morphism_g)) => {
-            if morphism_f == morphism_g
-                || category.equalities.check_equality(morphism_f, morphism_g)
-            {
-                Box::new(std::iter::once(Bindings::new()))
-            } else {
-                Box::new(vec![].into_iter())
-            }
-        }
-        (Some(morphism_f), None) => Box::new(
-            category
-                .equalities
-                .get_equalities(morphism_f)
-                .chain(std::iter::once(morphism_f))
-                .map(|id| Bindings::single_morphism(morphism_g.clone(), id)),
-        ),
-        (None, Some(morphism_g)) => Box::new(
-            category
-                .equalities
-                .get_equalities(morphism_g)
-                .chain(std::iter::once(morphism_g))
-                .map(|id| Bindings::single_morphism(morphism_f.clone(), id)),
-        ),
-        (None, None) => Box::new(
-            category
-                .equalities
-                .all_equalities()
-                .chain(category.morphisms.iter().map(|(&id, _)| (id, id)))
-                .map(|(f, g)| {
-                    let mut binds = Bindings::new();
-                    binds.bind_morphism(morphism_f.clone(), f);
-                    binds.bind_morphism(morphism_g.clone(), g);
-                    binds
-                }),
-        ),
-    }
+    let constraints = vec![morphism_f, morphism_g]
+        .into_iter()
+        .map(|label| (label.clone(), bindings.get_morphism(label)))
+        .collect::<Vec<_>>();
+
+    Box::new(
+        category
+            .equalities
+            .all_equalities()
+            .filter_map(move |(f, g)| {
+                constraint_unordered(constraints.iter().cloned(), vec![f, g])
+                    .map(|binds| Bindings::from_morphisms(binds))
+            }),
+    )
 }
