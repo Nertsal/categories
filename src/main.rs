68910d4e1006508816ee087fd7af6a2615cd5e7c
Nertsal <sasha.kudasov04@gmail.com>
2022-03-06 13:19:33 +0300

    Update geng

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,12 @@ fn main() {
     logger::init().unwrap();
     geng::setup_panic_handler();
 
-    // Setup working directory
-    if let Some(dir) = std::env::var_os("CARGO_MANIFEST_DIR") {
-        std::env::set_current_dir(std::path::Path::new(&dir).join("static")).unwrap();
-    } else {
-        #[cfg(not(target_arch = "wasm32"))]
-        {
-            if let Some(path) = std::env::current_exe().unwrap().parent() {
-                std::env::set_current_dir(path).unwrap();
-            }
-        }
-    }
-
     let geng = Geng::new_with(geng::ContextOptions {
         title: "Categories".to_owned(),
         max_delta_time: 0.05,
         ..default()
     });
-    let assets = <Assets as geng::LoadAsset>::load(&geng, ".");
+    let assets = <Assets as geng::LoadAsset>::load(&geng, &static_path());
 
     geng::run(
         &geng,
